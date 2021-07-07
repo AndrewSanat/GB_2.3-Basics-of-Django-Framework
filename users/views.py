57d86acfb64080204e14c9be7425fc421908592d
sorokinandan <sorokinandan@gmail.com>
2021-07-07 22:27:52 +0300

    2.1. Кастомирирована форма аутентификации

--- users/views.py
+++ users/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render
+from users.forms import UserLoginForm
 
 # Create your views here.
+
 def login(request):
-    return render(request, 'users/login.html')
+    context = {
+        'title': 'GeekShop - Авторизация',
+        'form': UserLoginForm
+    }
+    return render(request, 'users/login.html', context)
 
 
 def registration(request):
     return render(request, 'users/registration.html')
+
