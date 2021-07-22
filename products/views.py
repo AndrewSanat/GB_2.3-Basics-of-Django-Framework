012f464d53fc9cdce816b7a1e8cfd5e12107437f
sorokinandan <sorokinandan@gmail.com>
2021-07-22 19:19:16 +0300

    2. Добавлен постраничный вывод товаров в каталоге.

--- products/views.py
+++ products/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from datetime import datetime
 from products.models import Product, ProductCategory
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 # Create your views here.
 # Контроллеры:
@@ -15,7 +16,7 @@ def index(request):
     }
     return render(request, 'products/index.html', context)
 
-def products(request, category_id=None):
+def products(request, category_id=None, page=1):
     context = {
         'title': 'GeekShop - Каталог',
         'navigationText': 'GeekShop',
@@ -24,7 +25,15 @@ def products(request, category_id=None):
         'categories': ProductCategory.objects.all()
     }
     if category_id:
-        context['products'] = Product.objects.filter(category_id=category_id)
+        products = Product.objects.filter(category_id=category_id)
     else:
-        context['products'] = Product.objects.all()
+        products = Product.objects.all()
+    paginator = Paginator(products, 3)
+    try:
+        products_paginator = paginator.page(page)
+    except PageNotAnInteger:
+        products_paginator = paginator.page(1)
+    except EmptyPage:
+        products_paginator = paginator.page(paginator.num_pages)
+    context['products'] = products_paginator
     return render(request, 'products/products.html', context)
